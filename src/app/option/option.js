09ff3917600d3b1dda09de0776e0524cd06db644
null <null>
2022-10-17 01:47:30 +0300

    null

--- src/app/option/option.js
+++ src/app/option/option.js
@@ -1,20 +1,21 @@
 import getOrDefaultAsTxtFn from '../lib/option/getOrDefaultAsTxtFn.js';
 import resetAndGetAsTxtFn from '../lib/option/resetAndGetAsTxtFn.js';
-import saveTxtAndGetFn from '../lib/option/saveTxtAndGetFn.js';
+import saveTxtAndGetAsTxtFn from '../lib/option/saveTxtAndGetAsTxtFn.js';
 
 const mainFn = async () => {
     const option = document.getElementById('option');
 
     option.value = await getOrDefaultAsTxtFn();
     document.getElementById('reset').addEventListener('click', async () => {
-        option.value = await resetAndGetAsTxtFn();
+        try {
+            option.value = await resetAndGetAsTxtFn();
+        } catch (e) {
+            alert('json is incorrect');
+        }
     });
     document.getElementById('save').addEventListener('click', async () => {
-        await saveTxtAndGetFn(option.value);
-    });
-    option.addEventListener('input', async () => {
         try {
-            await saveTxtAndGetFn(option.value);
+            option.value = await saveTxtAndGetAsTxtFn(option.value);
         } catch (e) {
             alert('json is incorrect');
         }
