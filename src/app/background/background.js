b8c1a1be1273bb0a99884928fbfe16d165805483
null <null>
2022-10-16 12:20:49 +0300

    null

--- src/app/background/background.js
+++ src/app/background/background.js
@@ -1,41 +1,42 @@
 import openAsPngFn from "../../lib/openAsPngFn.js";
 import saveAsPngFn from "../../lib/saveAsPngFn.js";
 
-browser.browserAction.onClicked.addListener(async ({id: tabId}) => {
-    const zoomOutRate = 4;
-    const zoomOutRateDelay = 2000;
-    const qualityRate = 1;
-    const css = {
-        code: `body { transform-origin: 0 0; transform: scale(${1 / zoomOutRate}); }`,
-        allFrames: true,
-        cssOrigin: 'user',
-        matchAboutBlank: true,
-        runAt: 'document_start'
-    };
+const onClickedListenerBuilderFn = (zoomOutRate, zoomOutRateDelay, qualityRate) => {
+    return async ({id: tabId}) => {
+        const css = {
+            code: `body { transform-origin: 0 0; transform: scale(${1 / zoomOutRate}); }`,
+            allFrames: true,
+            cssOrigin: 'user',
+            matchAboutBlank: true,
+            runAt: 'document_start'
+        };
 
-    await browser.tabs.insertCSS(css);
-    await new Promise(resolve => setTimeout(resolve, zoomOutRateDelay));
+        await browser.tabs.insertCSS(css);
+        await new Promise(resolve => setTimeout(resolve, zoomOutRateDelay));
 
-    const {width, height, scale} = await browser.tabs.sendMessage(tabId, null);
-    const screenshot = await browser.tabs.captureTab(
-        tabId,
-        {
-            rect: {
-                x: 0,
-                y: 0,
-                width,
-                height
-            },
-            scale: scale * zoomOutRate * qualityRate
-        }
-    );
+        const {width, height, scale} = await browser.tabs.sendMessage(tabId, null);
+        const screenshot = await browser.tabs.captureTab(
+            tabId,
+            {
+                rect: {
+                    x: 0,
+                    y: 0,
+                    width,
+                    height
+                },
+                scale: scale * zoomOutRate * qualityRate
+            }
+        );
 
-    await browser.tabs.removeCSS(css);
+        await browser.tabs.removeCSS(css);
 
-    const screenshotBlob = await (await fetch(screenshot)).blob();
+        const screenshotBlob = await (await fetch(screenshot)).blob();
 
-    await Promise.allSettled([
-        openAsPngFn(screenshotBlob),
-        saveAsPngFn(screenshotBlob, `screenshot_${new Date().toISOString().replaceAll(':', '_')}.png`)
-    ]);
-});
+        await Promise.allSettled([
+            openAsPngFn(screenshotBlob),
+            saveAsPngFn(screenshotBlob, `screenshot_${new Date().toISOString().replaceAll(':', '_')}.png`)
+        ]);
+    }
+};
+
+browser.browserAction.onClicked.addListener(onClickedListenerBuilderFn(4, 2000, 4));
