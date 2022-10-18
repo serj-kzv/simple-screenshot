03f2b1e606f15861c3c9a47bac5d65810bddd5d2
null <null>
2022-10-18 11:56:43 +0300

    null

---
+++ src/app/bookmark/simpleScreenshotLevel8.js
@@ -0,0 +1,5 @@
+import makeSimpleScreenshotFn from './makeSimpleScreenshotFn.js';
+
+const main = async () => await makeSimpleScreenshotFn(8);
+
+main();
--- src/app/option/makeSimpleScreenshotBookmarksFn.js
+++ src/app/option/makeSimpleScreenshotBookmarksFn.js
@@ -6,11 +6,17 @@ const makeSimpleScreenshotBookmarksFn = async () => {
     const title1 = `SmplScrsht X1 Simple Screenshot Extension Bookmark - ${uuid}`;
     const title2 = `SmplScrsht X2 Simple Screenshot Extension Bookmark - ${uuid}`;
     const title4 = `SmplScrsht X4 Simple Screenshot Extension Bookmark - ${uuid}`;
+    const title6 = `SmplScrsht X6 Simple Screenshot Extension Bookmark - ${uuid}`;
+    const title8 = `SmplScrsht X8 Simple Screenshot Extension Bookmark - ${uuid}`;
+    const title10 = `SmplScrsht X10 Simple Screenshot Extension Bookmark - ${uuid}`;
 
     await Promise.allSettled([
         ...(await browser.bookmarks.search({title: title1})).map(async ({id}) => await browser.bookmarks.remove(id)),
         ...(await browser.bookmarks.search({title: title2})).map(async ({id}) => await browser.bookmarks.remove(id)),
-        ...(await browser.bookmarks.search({title: title4})).map(async ({id}) => await browser.bookmarks.remove(id))
+        ...(await browser.bookmarks.search({title: title4})).map(async ({id}) => await browser.bookmarks.remove(id)),
+        ...(await browser.bookmarks.search({title: title6})).map(async ({id}) => await browser.bookmarks.remove(id)),
+        ...(await browser.bookmarks.search({title: title8})).map(async ({id}) => await browser.bookmarks.remove(id)),
+        ...(await browser.bookmarks.search({title: title10})).map(async ({id}) => await browser.bookmarks.remove(id))
     ]);
     await Promise.allSettled((await browser.bookmarks.search({title})).map(async ({id}) => await browser.bookmarks.remove(id)));
 
@@ -33,6 +39,21 @@ const makeSimpleScreenshotBookmarksFn = async () => {
         parentId,
         title: title4,
         url: browser.runtime.getURL("src/app/bookmark/simpleScreenshotLevel4.html"),
+    });
+    await browser.bookmarks.create({
+        parentId,
+        title: title6,
+        url: browser.runtime.getURL("src/app/bookmark/simpleScreenshotLevel6.html"),
+    });
+    await browser.bookmarks.create({
+        parentId,
+        title: title8,
+        url: browser.runtime.getURL("src/app/bookmark/simpleScreenshotLevel8.html"),
+    });
+    await browser.bookmarks.create({
+        parentId,
+        title: title10,
+        url: browser.runtime.getURL("src/app/bookmark/simpleScreenshotLevel10.html"),
     });
 };
 
